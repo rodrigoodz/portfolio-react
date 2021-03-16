0d04636c8ebce416def14470dbd47e23cdd5b229
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-16 18:50:55 -0300

    creado componente AboutMe.js

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div className="flex h-screen bg-green-800">
+      <h1>asdasdsad</h1>
+    </div>
+  );
+};
+
+export default AboutMe;
