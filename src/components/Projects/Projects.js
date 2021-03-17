9c76ecb5f5f380bf9a7c5dde6594bb74ac19d02f
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-17 11:11:51 -0300

    diseño basico Projects

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div
+      className="flex xl:flex-row flex-col flex-1 md:h-full  bg-green-800  justify-center items-center  md:p-20 p-4  relative z-10 "
+      id="projects"
+    >
+      <h1>asd</h1>
+    </div>
+  );
+};
+
+export default Projects;
