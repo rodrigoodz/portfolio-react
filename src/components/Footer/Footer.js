d726d26e8a871a4bcd88cb7d32213941a41d7153
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-19 16:47:34 -0300

    mejorado diseño responsive del footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,9 @@ import Contact from "./Contact";
 
 const Footer = () => {
   return (
-    <div className="flex xl:flex-row flex-col flex-1 md:h-full  bg-gray-800  justify-between items-center  md:p-20 p-4  relative z-10 border-t-2  border-gray-700">
-      <h3 className="text-white font-bold">
-        Diseño y Desarrollo hecho por Rodrigo Odz - 2021
+    <div className="flex xl:flex-row flex-col-reverse flex-1 md:h-full  bg-gray-800  justify-between items-center  xl:p-6 p-4  relative z-10 border-t-2  border-gray-700">
+      <h3 className="text-white pt-2 font-bold text-center md:text-lg  text-sm">
+        Diseñado y Desarrollado por Rodrigo Odz - 2021
       </h3>
       <Contact />
     </div>
