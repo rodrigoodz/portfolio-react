7ec6331f77077a913bbd4ecd3eb9040e5df3d7b2
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-19 13:34:41 -0300

    eliminad stroke de los svg

--- src/components/Header/DropdownIcon.js
+++ src/components/Header/DropdownIcon.js
@@ -11,12 +11,7 @@ const DropdownIcon = ({ toggle, isOpen }) => {
           stroke="white"
           className="w-6 h-6"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
+          <path d="M6 18L18 6M6 6l12 12" />
         </svg>
       ) : (
         <svg
@@ -26,12 +21,7 @@ const DropdownIcon = ({ toggle, isOpen }) => {
           stroke="white"
           className="w-6 h-6"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          />
+          <path d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       )}
     </div>
