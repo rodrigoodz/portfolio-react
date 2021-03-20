5e93b605d66b693b3684d51399dec5d2ffe23b8c
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-19 22:40:00 -0300

    mejorado grilla responsive

--- src/components/FiveColGridResponsive.js
+++ src/components/FiveColGridResponsive.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FiveColGridResponsive = ({ children }) => {
   return (
-    <div className="grid md:grid-cols-10 grid-cols-4  p-10 md:gap-32 gap-4 ">
+    <div className="grid xl:grid-cols-10 lg:grid-cols-8 sm:grid-cols-6 grid-cols-4  p-10 md:gap-32 gap-4  md:place-items-center place-items-auto">
       {children}
     </div>
   );
